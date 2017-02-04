f45d4ffb71dd4009e021240639d6650d7515f239
clair13 <theclairvoyant1313@yahoo.gr>
2017-02-04 15:18:57 +0100

    Add datetime picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= reuire jquery-ui/datepicker
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
+// require turbolinks
 //= require_tree .
