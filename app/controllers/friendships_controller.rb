4903cebdf2125115964b9c98c5e3ae559713e27c
clair13 <theclairvoyant1313@yahoo.gr>
2017-02-04 20:58:27 +0100

    Implementedunfollow friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -13,6 +13,18 @@ class FriendshipsController < ApplicationController
     Friendship.create(friendship_params) unless current_user.follows_or_same?(friend)
     redirect_to root_path
   end
+
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    friendname = @friendship.friend.full_name
+    
+    if @friendship.destroy
+      flash[:notice] = "#{friendname} unfollowed."
+    else
+      flash.now[:alert] = "#{friendname} could not be unfollowed."
+    end
+    redirect_to user_exercises_path(current_user)
+  end
   
   private
   
