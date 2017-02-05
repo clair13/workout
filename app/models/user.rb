da4f7056af3b8c8b03b72d7fac54e328dcba31c5
clair13 <theclairvoyant1313@yahoo.gr>
2017-02-05 14:47:07 +0100

    Create chatroom

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   has_many :exercises
   has_many :friendships
   has_many :friends, through: :friendships, class_name: "User"
+  has_one :room
   
   validates :first_name, presence: true
   validates :last_name, presence: true
+  
+  after_create :create_chatroom
   
   self.per_page = 10
   
@@ -30,12 +33,19 @@ class User < ApplicationRecord
         "%#{names_array[1]}%").order(:first_name)
     end
   end
-
+  
   def follows_or_same?(new_friend)
     friendships.map(&:friend).include?(new_friend) || self == new_friend
   end
-
+  
   def current_friendship(friend)
     friendships.where(friend: friend).first
+  end
+  
+  private
+  
+  def create_chatroom
+    hyphenated_username = self.full_name.split.join('-')
+    Room.create(name: hyphenated_username, user_id: self.id)
   end
 end
